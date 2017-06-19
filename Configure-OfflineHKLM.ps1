d40b011162a18eec0c97028b4e0dc0b301b916dc
Joakim Olsson <joakim.olsson@addskills.com>
2017-06-19 21:08:40 +0200

    Fixed bugs in CAF-GuestHost and Rearm-VMs. The Credentialsentries should not have 'VMs' key specified as Wildcard-expressions, but use regular expressions instead.

--- Configure-OfflineHKLM.ps1
+++ Configure-OfflineHKLM.ps1
@@ -17,7 +17,7 @@ function Configure-OfflineHKLM {
     if ( !($r | ? { $_ -match "CAFSetup$" }) ) {
             
         shoutOut "Setting up local CAF..." Cyan
-        $CAFAutorunBootstrap =  { start Powershell -Verb RunAs -ArgumentList '-WindowStyle Hidden -ExecutionPolicy Bypass -NoProfile -Command echo Bootstrap; echo $Env:USERNAME; iex (gp HKLM:\SOFTWARE\CAFSetup AutorunScript | % { `$_.AutorunScript })' }
+        $CAFAutorunBootstrap =  { start Powershell -Verb RunAs -ArgumentList '-WindowStyle Hidden -ExecutionPolicy Bypass -NoProfile -Command echo Bootstrap; echo $Env:USERNAME; iex (gp HKLM:\SOFTWARE\CAFSetup AutorunScript | % { $_.AutorunScript })' }
         $CAFAutorunScript = { echo ('Running CAFAutorun as {0}'-f ${Env:USERNAME}) ; ls C:\CAFAutorun | ? { $_.Name -match '.bat|.ps1' } | % { try{ & $_.FullName *>&1 } catch { Write-host $_ }  } }
             
         $operations = @(
