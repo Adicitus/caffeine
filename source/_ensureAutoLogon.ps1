4dc1459e4fc49f209eae3f50a0a0e2d3a326634c
Joakim Olsson <joakim.olsson@addskills.com>
2021-09-14 19:55:51 +0200

    Added logic to ensure AutoLogin account exists.

--- source/_ensureAutoLogon.ps1
+++ source/_ensureAutoLogon.ps1
@@ -6,9 +6,30 @@ function _ensureAutoLogon {
         $LogFile
     )
 
-    # $setup = Parse-ConfigFile $setupFile
-
     if ($autologon = $setup.'Credential-AutoLogin') {
+
+        if (!$autologon.domain -or ($autologon.domain -eq '.')) {
+            $user = Get-LocalUser -Name $autologon.username -ErrorAction SilentlyContinue
+
+            if (!$user) {
+                $params = @{
+                    $params.name = $autologon.username
+                }
+
+                if ($autologon.password) {
+                    $params.password = ConvertTo-SecureString -String $autologon.password -AsPlainText -Force
+                    $params.passwordNeverExpires = $true
+                } else {
+                    $params.noPassword = $true
+                }
+
+                $user = New-LocalUser @params -AccountNeverExpires
+                $adminsGroup = Get-LocalGroup -SID S-1-5-32-544
+
+                Add-LocalGroupMember -Group $adminsGroup -Member $user
+            }
+        }
+
         $templateFile = "$PSScriptRoot\.assets\templates\winlogon.tmplt.reg"
         $outputFile = "$tmpDir\winlogon.reg"
 
